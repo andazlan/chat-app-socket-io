51745a63d0148fe1672aa61b1ee110965efd1252
Andrew Azlan <azlanappleboy@gmail.com>
2018-03-30 15:52:41 +0700

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -17,13 +17,15 @@ socket.on('newMessage', function (message) {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
+
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from : 'User',
-        text : jQuery('[name=message]').val()
+        text : messageTextBox.val()
     }, function () { 
-
+        messageTextBox.val('');
     });
-    jQuery('[name=message]').val("");
 });
 
 socket.on('newLocationMessage', function (message) {  
@@ -41,12 +43,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {  
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         });
     }, function () {  
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
