314953e487bd4d4392e3b690fe5df5db6bc52dcc
Andrew Azlan <azlanappleboy@gmail.com>
2018-03-30 17:05:08 +0700

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Height
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 //when connect to server
 socket.on('connect', function () {
     console.log('connected to server');
@@ -19,6 +36,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 //show location message
@@ -31,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt : formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 //send chat message
